b90875c2494b1d170bc558a2bc64d777657c09a0
melonxi <zhaoxw1993@gmail.com>
2023-07-11 11:56:34 +0800

    两数之和注释

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -7,6 +7,9 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 使用哈希表，时间复杂度O(n)，空间复杂度O(n)
+        # 哈希表存储每个数字的下标
+        
 
         num_index_dict = {}
         for i,num in enumerate(nums):
