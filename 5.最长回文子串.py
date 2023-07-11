b3aa43fdb4eae9424a94dc1593be02b7f56c8530
melonxi <zhaoxw1993@gmail.com>
2023-07-11 12:35:18 +0800

    最长回文子串注释

--- 5.最长回文子串.py
+++ 5.最长回文子串.py
@@ -7,19 +7,21 @@
 # @lc code=start
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
+        #动态规划
+        #循环嵌套，考察左右边界，以及是否相等
+        n = len(s)#字符串长度
         if n < 2:
             return s
         max_len = 1
         start = 0
 
-        DP = [[False] * n for _ in range(n)]
+        DP = [[False] * n for _ in range(n)]#初始化DP数组
 
-        for i in range(n):
+        for i in range(n):#初始化DP数组
             DP[i][i] = True
         
-        for L in range(2,n+1):
-            for i in range(n-L+1):
+        for L in range(2,n+1):#L为子串长度
+            for i in range(n-L+1):#i为子串起始位置
                 j = i+L-1
                 if s[i]!=s[j]:
                     DP[i][j] = False
