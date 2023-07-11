dad3c59a16eef8f9ccced7c99f426d21623e45f8
melonxi <zhaoxw1993@gmail.com>
2023-07-11 11:48:14 +0800

    first

--- 207.课程表.py
+++ 207.课程表.py
@@ -36,6 +36,42 @@ class Solution:
                 if in_degree[cur] == 0:
                     queue.append(cur)
         return numCourses == 0
+        # 递归,DFS
+        # 1. 使用邻接表存储图
+        # 2. 从每个节点开始，判断是否有环
+        # 3. 如果有环，返回False，否则返回True
+        # 时间复杂度：O(n+m)，n为节点数，m为边数
+        # 空间复杂度：O(n+m)，n为节点数，m为边数
+        # 邻接表
+        adj = [[] for _ in range(numCourses)]
+        # 初始化邻接表
+        for cur, pre in prerequisites:
+            adj[pre].append(cur)
+        # 用于记录节点的状态，0表示未搜索，1表示搜索中，2表示已完成搜索
+        visited = [0] * numCourses
+        # 判断是否有环
+        def dfs(i):
+            # 如果已经搜索过，直接返回True
+            if visited[i] == 1:
+                return False
+            # 如果已经完成搜索，直接返回False
+            if visited[i] == 2:
+                return True
+            # 标记为搜索中
+            visited[i] = 1
+            # 遍历邻接表
+            for j in adj[i]:
+                # 如果有环，返回False
+                if not dfs(j):
+                    return False
+            # 标记为已完成搜索
+            visited[i] = 2
+            return True
+        # 从每个节点开始，判断是否有环
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
 
     
 # @lc code=end
